348961ca6f585561ea330580ea9cfd041fad29dc
Pulkit <Pulkit.kumar75@gmail.com>
2021-05-21 11:00:03 -0500

    example

---
+++ src/com/xyzcompany/ITEmployee/Exercise.java
@@ -0,0 +1,15 @@
+package com.xyzcompany.ITEmployee;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println(24+26);
+		  float Fahrenheit, Celsius;  
+          Celsius= 13;  
+          Fahrenheit =((Celsius*9)/5)+32;  
+          System.out.println("Temperature in Fahrenheit is: "+Fahrenheit);
+
+	}
+
+}
